bef716816717839fe53eea5249ce223c26f303bb
Casey <innerforest7@gmail.com>
2020-09-15 23:13:54 -0700

    add rightArrow

--- react/src/components/EPICimage.js
+++ react/src/components/EPICimage.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import ChevronRight from 'react';
+// import spaceArrow from '../images/SpaceArrow';
 
 const EPICimage = (props) => {
+  const API_KEY = "DZlJvpOuxIYWGgRha1mCvDtqDwngAsgkv09kyCKz"
+  let resultArray = props.epicResult.map(identifier => 
+    // console.log(identifier.image))
+    `https://api.nasa.gov/EPIC/archive/natural/2019/05/30/png/${identifier.image}.png?api_key=${API_KEY}`)
+     
+  // console.log("resultArray===>", identifier.image)
+  // let resultArray = props.epicResult[0].image;
   return (
     <>
+      {/* <img id={"chevron-right"} src={'../images/spaceArrow.png'} alt={""}></img> */}
+      <div id={"chevron-right"} ></div>
       {props.epicResult !== undefined ?
-      <>
-        <p>{props.epicResult[0].image}</p>
-          <img className={"potd-img"} src={`https://api.nasa.gov/EPIC/archive/natural/2019/05/30/png/${props.epicResult[0].image}.png?api_key=DEMO_KEY`} alt={""}></img>
+      <div className={"epic-img-c"}>
+        {}
+          <img className={"epic-img-c__img"} src={resultArray[0]} alt={""}></img>
         {/* `https://api.nasa.gov/EPIC/archive/natural/2019/05/30/png/${props.epicResult[0].image}.png?api_key=DEMO_KEY` */}
-      </>
+      </div>
         :
-        <p>not showing</p>
-        // <img className={"potd-img"} src={''} alt={""}></img>
+        <img className={"potd-img"} src={''} alt={""}></img>
+        
+
       }
     </>
   )
---
+++ react/src/images/ChevronRight.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const ChevronRight = (props) => {
+  return (
+    <svg id={"chevron-right"} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 141.44 51.94"><g id="Layer_2" data-name="Layer 2"><g id="Layer_3" data-name="Layer 3"><polygon points="141.44 51.95 108.81 51.95 70.72 23.97 32.64 51.95 0 51.95 70.72 0 141.44 51.95" /></g></g></svg>
+  )
+}
+export default ChevronRight;
