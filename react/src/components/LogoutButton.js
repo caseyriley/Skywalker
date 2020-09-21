ac286679fd2b8b72a434eb74347e5fd91a2a5af5
Casey <innerforest7@gmail.com>
2020-09-20 22:21:17 -0700

    change logout button for heroku refresh

--- react/src/components/LogoutButton.js
+++ react/src/components/LogoutButton.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const LogoutButton = (props) => {
   return (
-    <div className={`logout-button ${props.bottomNavState ? "visible" : "none"}`} onClick={() => { localStorage.removeItem("auth_token"); window.location.href = 'http://localhost:3000' }}>
+    <div className={`logout-button ${props.bottomNavState ? "visible" : "none"}`} onClick={() => { 
+      localStorage.removeItem("auth_token"); 
+      window.location.href = 'https://skywalker3.herokuapp.com' 
+      // window.location.href = 'http://localhost:3000' 
+      }}>
       <span>log out</span>
     </div>
   )
