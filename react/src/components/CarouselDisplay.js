9941ec44c33bff2b90889843c95af98968bfab61
Casey <innerforest7@gmail.com>
2020-10-03 21:43:33 -0700

    set setInterval time to 5000

--- react/src/components/CarouselDisplay.js
+++ react/src/components/CarouselDisplay.js
@@ -2,6 +2,36 @@ import React, { useState, useEffect } from 'react';
 
 const CarouselDisplay = (props) => {
 
+  useEffect(() => {
+    // console.log(process.env.REACT_APP_API_KEY);  
+    // fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + process.env.REACT_APP_API_KEY + '&tags=' + searchString + '&per_page=' + numPics + '&page=1&format=json&nojsoncallback=1')
+    //   .then(function (response) {
+    //     return response.json();
+    //   })
+    //   .then(function (j) {
+
+        // const lookupPic = function (index) {
+        //   let pic = j.photos.photo[index];
+        //   return 'https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '.jpg';
+        // }
+  //       let picCount = 0;
+
+  //       setPic(lookupPic(picCount));
+  //       picCount++;
+  //       setInterval(() => {
+  //         setBlurState(true);
+  //         setTimeout(() => {
+  //           setPic(lookupPic(picCount));
+  //         }, 150);
+
+  //         setTimeout(() => {
+  //           setBlurState(false);
+  //         }, 500);
+  //         // setBlurState(false);
+  //         picCount++;
+  //       }, 5000);
+  //     })
+  // }, [searchArrayState]);
   // let userGalleryArray = props.userGalleryState ? props.userGalleryState : [];
 
   // const [opacityState, setOpacityState] = useState(false);
@@ -15,23 +45,25 @@ const CarouselDisplay = (props) => {
   // const [currentImageState, setCurrentImageState] = useState(0);
   // const [nextImageState, setNextImageState] = useState(1);
   // const [timeoutState, setTimeoutState] = useState(1);
-  const [carouselNextState, setCarouselNextState] = useState(77);
-  useEffect(()=> {
-    if (props.carouselToggleState){
-      let count = props.userGalleryArray.length;
-      const carouselElement = document.getElementById("carousel-img-c")
-      let child = document.querySelector(`#carousel-img-c div:nth-child(${count})`)
-      console.log(child)
-      setInterval(() => {
-        child.classList.add("opacity-zero")
-        count --;
-        setCarouselNextState(carouselNextState + 2)
-      }, 3000);
-    }
-    
 
+
+
+  // --------------------------
+  // const [carouselNextState, setCarouselNextState] = useState(77);
+  // useEffect(()=> {
+  //   if (props.carouselToggleState){
+  //     let count = props.userGalleryArray.length -1;
+  //     const carouselElement = document.getElementById("carousel-img-c")
+  //     let child = document.querySelector(`#carousel-img-c div:nth-child(${count})`)
+  //     console.log(child)
+  //     setInterval(() => {
+  //       child.classList.add("opacity-zero")
+  //       count --;
+  //       setCarouselNextState(carouselNextState + 2)
+  //     }, 5000);
+  //   }
   }, [props.carouselToggleState])
-
+// ---------------------------------
 
 
   return (
