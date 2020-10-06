7f648ebb80dcad245d4570549b5ce221c7ff3e67
Casey <innerforest7@gmail.com>
2020-10-05 23:20:37 -0700

    add carousel fade and rotate functionallity

--- react/src/components/CarouselDisplay.js
+++ react/src/components/CarouselDisplay.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import useScript from './useScript';
 
-const CarouselDisplay = (props) => {
 
-  useEffect(() => {
-    // console.log(process.env.REACT_APP_API_KEY);  
-    // fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + process.env.REACT_APP_API_KEY + '&tags=' + searchString + '&per_page=' + numPics + '&page=1&format=json&nojsoncallback=1')
-    //   .then(function (response) {
-    //     return response.json();
-    //   })
-    //   .then(function (j) {
 
-        // const lookupPic = function (index) {
-        //   let pic = j.photos.photo[index];
-        //   return 'https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '.jpg';
-        // }
+const CarouselDisplay = (props, items) => {
+
+  // useEffect(() => {
+  //   console.log(process.env.REACT_APP_API_KEY);  
+  //   fetch('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + process.env.REACT_APP_API_KEY + '&tags=' + searchString + '&per_page=' + numPics + '&page=1&format=json&nojsoncallback=1')
+  //     .then(function (response) {
+  //       return response.json();
+  //     })
+  //     .then(function (j) {
+
+  //       const lookupPic = function (index) {
+  //         let pic = j.photos.photo[index];
+  //         return 'https://farm' + pic.farm + '.staticflickr.com/' + pic.server + '/' + pic.id + '_' + pic.secret + '.jpg';
+  //       }
   //       let picCount = 0;
 
   //       setPic(lookupPic(picCount));
@@ -62,8 +65,28 @@ const CarouselDisplay = (props) => {
   //       setCarouselNextState(carouselNextState + 2)
   //     }, 5000);
   //   }
-  }, [props.carouselToggleState])
+  // }, [props.carouselToggleState])
 // ---------------------------------
+
+  const [itemsState, setItemsState] = useState([]);
+  useEffect(()=>{
+    for (let i = props.userGalleryArray.length - 1; i > -1; i--) {
+      let galleryItem = props.userGalleryArray[i];
+      setItemsState(prevResults => {
+        return [...prevResults, <div className={`carousel-background`} key={`${galleryItem.media} ${Math.floor(Math.random() * Math.floor(1000))}`}><img className={`carousel-img-1`} src={galleryItem.media} alt={""} /></div>]} 
+      )
+    }
+  }, [props.carouselToggle])
+
+
+  useEffect(()=>{
+    let stage = document.getElementById("carousel-img-c");
+    let fadeComplete = function (e) { stage.appendChild(arr[0]); };
+    let arr = document.getElementsByClassName("carousel-background");
+    for (let i = 0; i < arr.length; i++) {
+      arr[i].addEventListener("animationend", fadeComplete, false);
+    }
+  })
 
 
   return (
@@ -71,38 +94,16 @@ const CarouselDisplay = (props) => {
       {props.carouselToggleState ?
         <>
           <div id={"carousel-img-c"} onClick={props.carouselToggle}>
-          {props.userGalleryArray.map((galleryItem, index) => {
-            return (
-              
-              <div className={`carousel-background`} key={`${galleryItem.media} ${Math.floor(Math.random() * Math.floor(1000))}`}>
-                <img className={`carousel-img-1`} src={galleryItem.media} alt={""} />
-              </div>
-            )
-          }) 
-            
-          }
+            {itemsState}
           </div>
-          {/* <div className={"carousel-img-c"} onClick={props.carouselToggle}>
-
-            <div className={`carousel-background`}>
-              <img className={`carousel-img-1`} src={props.userGalleryArray[props.nextImageState].media} alt={""}  />
-            </div>
-
-            <div className={`carousel-background  ${props.opacityState ? "opacity-zero" : "opacity-100"}` }>
-              <img className={`carousel-img-2`} src={props.userGalleryArray[props.currentImageState].media} alt={""}/>
-            </div>
-
-          </div> */}
         </>
         :
         <>
         </>
+        
       }
-      {/* <div className={"carousel-c"} onClick={carouselToggle}>
-        <img className={"carousel"} src={carousel} alt={""} ></img>
-        <img className={"carousel-saturn"} src={saturn} alt={""} ></img>
-      </div> */}
     </>
   )
+
 }
 export default CarouselDisplay;
