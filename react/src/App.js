a68782a58ebdf9e67957d09f13d1c66c2b6e70f3
Casey <innerforest7@gmail.com>
2020-10-07 17:55:45 -0700

    change favicon

--- react/src/App.js
+++ react/src/App.js
@@ -23,4 +23,3 @@ function App() {
 export default App;
 
 
-//^^^^
--- react/src/clicklog.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("DOMContentLoaded", event =>{
-  window.addEventListener("click", event =>{
-    console.log(event.target)
-  })
-})
