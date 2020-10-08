b52274920e2c815a978ee8842415e0515c424495
Casey <innerforest7@gmail.com>
2020-10-07 17:30:54 -0700

    comment back in favicon route

--- skywalkerBackend/__init__.py
+++ skywalkerBackend/__init__.py
@@ -61,10 +61,10 @@ def api():
 
 
 
-# @app.route('/favicon.ico')
-# def favicon():
-#     return send_from_directory(os.path.join(app.root_path, 'static'),
-#                           'favicon.ico',mimetype='image/vnd.microsoft.icon')
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
 
 # @app.route('/api/favicon.ico')
